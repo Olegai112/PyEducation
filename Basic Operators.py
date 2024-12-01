42210724674a702a006946473aaac1739b9063b3
Олег <frussia35@yandex.ru>
2024-12-01 08:15:33 +0300

    Basic Operators

---
+++ Basic Operators.py
@@ -0,0 +1,16 @@
+# Целые числа
+a = 10
+b = 5
+print(a + b)  # Вывод: 15
+print(a - b)  # Вывод: 5
+print(a * b)  # Вывод: 50
+print(a / b)  # Вывод: 2.0
+print(a // b)  # Вывод: 3
+print(a % b)   # Вывод: 1
+print(a ** b)   # Вывод: 100000
+print(a == b)  # Вывод: False
+print(a != b)  # Вывод: True
+print(a > b)   # Вывод: True
+print(a < b)   # Вывод: False
+print(a >= b)  # Вывод: True
+print(a <= b)  # Вывод: False
